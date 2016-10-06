f6e6d1257ca1396a76cb0c12088a720c102c5d79
gg <fountainoy@icloud.com>
2016-10-06 18:43:25 +0800

    PC

--- Face_keyPoint_train_cnn.py
+++ Face_keyPoint_train_cnn.py
@@ -1,34 +1,32 @@
-import tensorflow as tf
-import pandas as pd
-import numpy as np
-import Face_kyePoint_cnn
-BATCH_SIZE = 128
+from Face_kyePoint_cnn import *
+from Global_defintion import *
+
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('max_steps', 7000,
+tf.app.flags.DEFINE_integer('max_steps', 25000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_string('summary_dir', '/tmp/faceKeypoint',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 
 
-
 def train():
-    train_datas, train_labels, validation_datas, validation_labels = Face_kyePoint_cnn.split_data()
+    train_datas, train_labels, validation_datas, validation_labels = split_data()
     epochs_completed = 0
     index_in_epoch = 0
     num_examples = train_datas.shape[0]
     with tf.Graph().as_default():
-        x = tf.placeholder('float', shape=[None, Face_kyePoint_cnn.INPUT_SIZE])
-        y_ = tf.placeholder('float', shape=[None, Face_kyePoint_cnn.LABEL_SIZE])
+        x = tf.placeholder('float', shape=[None,  INPUT_SIZE])
+        y_ = tf.placeholder('float', shape=[None,  LABEL_SIZE])
+        keep_prob = tf.placeholder('float')
 
-        model_labels, regularizers = Face_kyePoint_cnn.inference(x)
+        model_labels = inference(x, keep_prob)
 
-        loss = Face_kyePoint_cnn.loss(model_labels, y_, regularizers)
+        loss_op = loss(model_labels, y_)
 
-        optimizer = Face_kyePoint_cnn.train(loss)
+        optimizer = train_in_cnn(loss_op)
 
-        tf.scalar_summary("loss",loss)
+        tf.scalar_summary("loss", loss_op)
 
         init = tf.initialize_all_variables()
         sess = tf.Session()
@@ -38,11 +36,12 @@ def train():
         validation_writer = tf.train.SummaryWriter(FLAGS.summary_dir + '/validation')
 
     for step in range(FLAGS.max_steps):
-        batch_x, batch_y, index_in_epoch, epochs_completed,train_datas,train_labels = Face_kyePoint_cnn.get_batch(BATCH_SIZE, train_datas, train_labels, index_in_epoch, epochs_completed, num_examples)
-        sess.run([optimizer], feed_dict={x: batch_x, y_: batch_y})
+        batch_x, batch_y, index_in_epoch, epochs_completed,train_datas,train_labels = \
+            get_batch(BATCH_SIZE, train_datas, train_labels, index_in_epoch, epochs_completed, num_examples)
+        sess.run([optimizer,merge], feed_dict={x: batch_x, y_: batch_y,keep_prob: 0.5})
+        train_writer.add_summary(sess.run(merge, feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.0}), step)
         if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-            validation_writer.add_summary(sess.run(merge, feed_dict={x: validation_datas[0:Face_kyePoint_cnn.VALIDATION_SIZE], y_: validation_labels[0:Face_kyePoint_cnn.VALIDATION_SIZE]}), step)
-            train_writer.add_summary(sess.run(merge, feed_dict={x: batch_x, y_: batch_y}), step)
+            validation_writer.add_summary(sess.run(merge, feed_dict={x: validation_datas, y_: validation_labels,keep_prob: 1.0}), step)
         if step == (FLAGS.max_steps - 1):
             print (epochs_completed)
 
--- Face_kyePoint_cnn.py
+++ Face_kyePoint_cnn.py
@@ -1,99 +1,84 @@
 import tensorflow as tf
 import pandas as pd
-import numpy as np
+from Global_defintion import *
+from Tools import *
 
-VALIDATION_SIZE = 100
-INPUT_SIZE = 9216
-LABEL_SIZE = 30
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('regular', '0', """if use regularization to prevent overfitting. 0 = not use""")
-LEARNING_RATE = 0.01
-MOMTENMUM = 0.9
+tf.app.flags.DEFINE_string('loss_and_L2', 'lossandL2', """ set a collection name""")
 
 
 def split_data():
     datas = pd.read_csv('training.csv').dropna()
-
-    images = np.vstack(datas['Image'].apply(lambda im: np.fromstring(im, sep=' ') / 255.0).values).astype(np.float32).reshape(-1, INPUT_SIZE)
-   # images = np.vstack(images.values)
-    #images = images.reshape(-1, 96, 96, 1)
-    labels = (datas[datas.columns[:-1]].values - 48) / 48
-    #shuffle
-    perm = np.arange(images.shape[0])
-    np.random.shuffle(perm)
-    images = images[perm]
-    labels = labels[perm]
-    # split data into train&corss_validation
+    images = shuffle(np.vstack(datas['Image'].apply(lambda im: np.fromstring(im, sep=' ') / 255.0).values).astype(np.float32).reshape(-1, INPUT_SIZE))
+    labels = shuffle(datas[datas.columns[:-1]].values / 96)
+    # split data into train&cross_validation
     train_images = images[VALIDATION_SIZE:]
     train_labels = labels[VALIDATION_SIZE:]
     validation_images = images[:VALIDATION_SIZE]
     validation_labels = labels[:VALIDATION_SIZE]
-
     return train_images, train_labels, validation_images, validation_labels
 
 
-def inference(datas):
-    #kernel_size1 = [3, 3, 1, 32]
-    #kernel_size2 = [3, 3, 32, 64]
+def inference(datas, keep_prob):
+    kernel_size1 = [5, 5, 1, 32]
+    kernel_size2 = [5, 5, 32, 64]
+    kernel_size3 = [5, 5, 64, 128]
 
-    w1 = weight_variable([INPUT_SIZE, 100])
-    b1 = bias_variable([100])
-    hidden = tf.nn.relu(tf.matmul(datas,w1)+b1)
+    w1 = weight_variable(kernel_size1)
+    b1 = bias_variable([32])
+    pool_1 = max_pool_2x2(tf.nn.relu(conv(tf.reshape(datas, [-1, 96, 96, 1]), w1)+b1))
 
-    w2 = weight_variable([100, LABEL_SIZE])
-    b2 = bias_variable([LABEL_SIZE])
+    w2 = weight_variable(kernel_size2)
+    b2 = bias_variable([64])
+    pool_2 = max_pool_2x2(tf.nn.relu(conv(pool_1, w2)+b2))
 
-    labels = tf.matmul(hidden, w2) + b2
+    conv3_w = weight_variable(kernel_size3)
+    conv3_b = bias_variable([128])
+    pool_3 = max_pool_2x2(tf.nn.relu(conv(pool_2, conv3_w) + conv3_b))
 
-    if FLAGS.regular:
-        regularizer = tf.nn.l2_loss(w2)+tf.nn.l2_loss(b2)
-    else:
-        regularizer = 0
-    return labels, regularizer
+    w3 = weight_variable([12*12*128, 500])
+    b3 = bias_variable([500])
+
+    fc_1 = tf.nn.dropout(tf.nn.relu(tf.matmul(tf.reshape(pool_3, [-1, 12*12*128]), w3) + b3), keep_prob)
+
+    w4 = weight_variable([500, 30])
+    b4 = bias_variable([30])
+
+    labels = tf.matmul(fc_1, w4) + b4
+    return labels
 
 
-def loss(model_labels, labels, regularizers):
+def loss(model_labels, labels):
     loss = tf.reduce_mean(tf.reduce_sum(tf.square(model_labels - labels), 1))
+    tf.add_to_collection(FLAGS.loss_and_L2, loss)
    # loss = -tf.reduce_sum(labels*tf.log(model_labels))
-    if regularizers != 0:
-        loss += 5e-4 * regularizers
-    return loss
+    return tf.add_n(tf.get_collection(FLAGS.loss_and_L2))
 
 
-def train(loss):
-    train_step = tf.train.MomentumOptimizer(LEARNING_RATE,MOMTENMUM).minimize(loss)
+def train_in_cnn(loss_op):
+    train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss_op)
     return train_step
 
+
+# weight initialization with decay
+def weight_variable(shape, wd):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    if wd:
+        weight_decay = tf.mul(tf.nn.l2_loss(initial), wd)
+        tf.add_to_collection(FLAGS.loss_and_L2, weight_decay)
+    return tf.Variable(initial)
 
 # weight initialization
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
-
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
 
-def get_batch(batch_size, train_images, train_labels, index_in_epoch, epochs_completed, num_examples):
-    start = index_in_epoch
-    index_in_epoch += batch_size
-    # when all trainig data have been already used, it is reorder randomly
-    if index_in_epoch > num_examples:
-        # finished epoch
-        epochs_completed += 1
-        # shuffle the data
-        perm = np.arange(num_examples)
-        np.random.shuffle(perm)
-        train_images = train_images[perm]
-        train_labels = train_labels[perm]
-        # start next epoch
-        start = 0
-        index_in_epoch = batch_size
-        assert batch_size <= num_examples
-    end = index_in_epoch
-    return train_images[start:end], train_labels[start:end], index_in_epoch, epochs_completed, train_images, train_labels
+
 
 
 def max_pool_2x2(x):
@@ -102,5 +87,4 @@ def max_pool_2x2(x):
 
 def conv(datas, parameter):
     return tf.nn.conv2d(datas, parameter, strides=[1, 1, 1, 1], padding='SAME')
-
 
