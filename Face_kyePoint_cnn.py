42ba0514e9ed644601b41357b974955fdb684fa8
kakurong <fountainoy@iclou.com>
2016-11-07 18:31:02 +0800

    mac

--- Face_kyePoint_cnn.py
+++ Face_kyePoint_cnn.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-import pandas as pd
-from Global_defintion import *
-from Tools import *
+from utility.Model_Parameter import  *
+from utility.Tools import *
 
 FLAGS = tf.app.flags.FLAGS
 #tf.app.flags.DEFINE_string('loss_and_L2', 'lossandL2', """ set a collection name""")
@@ -53,6 +51,9 @@ def inference(datas, keep_prob):
     labels = tf.matmul(fc_2, fc3_w) + fc3_b
 
     return labels
+
+
+
 
 def inference_with_bn(datas, keep_prob,is_training):
     datas = tf.reshape(datas, [-1, 96, 96, 1])
@@ -145,46 +146,7 @@ def train_in_cnn(loss_op):
     return train_step
 
 
-# weight initialization with decay
-def weight_variable(shape, wd):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    if wd:
-        weight_decay = tf.mul(tf.nn.l2_loss(initial), wd)
-        tf.add_to_collection(FLAGS.loss_and_L2, weight_decay)
-    return tf.Variable(initial)
 
-# weight initialization
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-def conv(datas, parameter):
-    return tf.nn.conv2d(datas, parameter, strides=[1, 1, 1, 1], padding='SAME')
-
-
-
-def norm_layer(inputs,phase_train,scope = None):
-    #return tf.cond(phase_train,
-     #   lambda:tf.contrib.layers.batch_norm(inputs,scale=True,updates_collections=None,scope=scope, is_training = True),
-
-      #  lambda: tf.contrib.layers.batch_norm(inputs,scale=True,updates_collections=None,scope=scope,is_training=False))
-    if phase_train is not None:
-        return tf.contrib.layers.batch_norm(inputs, scale=True, updates_collections=None, is_training=True)
-    else:
-        return tf.contrib.layers.batch_norm(inputs, scale=True, updates_collections=None,
-                                             is_training=False)
 
 
 
--- Face_train_for_mac.py
+++ Face_train_for_mac.py
@@ -4,27 +4,23 @@ from utility.Global_defintion import *
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('max_steps', 50000,
+tf.app.flags.DEFINE_integer('max_steps', 30000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_string('summary_dir', '/tmp/faceKeypoint',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_string('model_dir', '/home/gg/PycharmProjects/Kaggle_face_keypoint/Model/',
+tf.app.flags.DEFINE_string('model_dir', '/Users/kakurong/PycharmProjects/Kaggle_face_keypoint/Model/',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 
-
-
 def train(if_train):
-        eye_center  = "eye_center"
+        #current_col = 4
+        #datas = pd.read_csv('training.csv')
         #train_datas_all, train_labels_all, validation_datas_all, validation_labels_all = data_argument()
-        datas = pd.read_csv('training.csv')
-
-        train_datas, train_labels, validation_datas, validation_labels = Deal_Image.DataCenter_eye(datas, 0, 3)
-
+        #train_datas, train_labels, validation_datas, validation_labels = dataArgument_withColumn(datas,current_col)
         epochs_completed = 0
         index_in_epoch = 0
-        num_examples = train_datas.shape[0]
+        num_examples = 100#train_datas.shape[0]
         #with tf.Graph().as_default():
         x = tf.placeholder('float', shape=[None,  INPUT_SIZE])
         y_ = tf.placeholder('float', shape=[None,  4])
@@ -38,6 +34,8 @@ def train(if_train):
         optimizer = train_in_cnn(loss_op)
 
         saver = tf.train.Saver()
+
+
         tf.scalar_summary("loss", loss_op)
         init = tf.initialize_all_variables()
         sess = tf.InteractiveSession()
@@ -48,41 +46,41 @@ def train(if_train):
         image_op = tf.image_summary('x-input',tf.reshape(x,[-1,96,96,1]),max_images=BATCH_SIZE)
         image_writer = tf.train.SummaryWriter(FLAGS.summary_dir + '/images')
         best_valid = np.inf
-        #load_model = saver.restore(sess, FLAGS.model_dir+ 'model.ckpteye_center-30000')
+        #load_model = saver.restore(sess, FLAGS.model_dir+ 'model.ckpt1')
         if if_train:
-
             #for current_col in range(31):
                 for step in range(FLAGS.max_steps):
-                    batch_x, batch_y, index_in_epoch, epochs_completed,inputs_new,labels_new = get_batch(BATCH_SIZE, index_in_epoch, epochs_completed, num_examples,train_datas,train_labels)
-                    if inputs_new != None:
-                        print ("ok")
-                        train_datas = inputs_new
-                        train_labels = labels_new
+                    batch_x, batch_y, index_in_epoch, epochs_completed,train_datas,train_labels = get_batch(BATCH_SIZE, train_datas, train_labels, index_in_epoch, epochs_completed, num_examples)
                     sess.run([optimizer,merge,image_op], feed_dict={x: batch_x, y_: batch_y,keep_prob: 0.5,phase_train:True})
                     train_writer.add_summary(sess.run(merge,feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.0,phase_train:True}), step)
                     validation_writer.add_summary(sess.run(merge,feed_dict={x: validation_datas, y_: validation_labels, keep_prob: 1.0,phase_train:False}), step)
                     loss_valid = sess.run(loss_op,feed_dict={x: validation_datas, y_: validation_labels, keep_prob: 1.0,phase_train:False})
-                    loss_train = sess.run(loss_op,feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.0,phase_train:False})
-
                     image_writer.add_summary(sess.run(image_op,feed_dict={x:batch_x}))
-                    print (step,loss_valid,loss_train)
+                    print (step,loss_valid)
                     if loss_valid < best_valid:
                        best_valid = loss_valid
                        best_valid_step = step
                     elif best_valid_step + EARY_STOP_PATIENCE < step:
                          print ("early stop at {},best loss was {}".format(best_valid_step,best_valid))
-                         saver.save(sess, FLAGS.model_dir + 'model.ckpt' + str(eye_center),global_step= best_valid_step+1)
-
+                         saver.save(sess, FLAGS.model_dir + 'model.ckpt' + str(current_col), global_step=step + 1)
                          break
-                saver.save(sess,FLAGS.model_dir + 'model.ckpt'+str(eye_center),global_step=step+1)
+                saver.save(sess,FLAGS.model_dir + 'model.ckpt'+str(current_col),global_step=step+1)
               #  current_col += 1
                # step = 0
                 #train_datas, train_labels, validation_datas, validation_labels = dataArgument_withColumn(datas,current_col)
 
         else:
-            #load_model = saver.restore(sess,'model.ckpt')
-            test_data,cols = load_test_data()
-            generate_submission(test_data,cols,sess,model_labels,x,keep_prob,phase_train)
+
+
+
+            IFLABELS = 1
+            image = Deal_Image.get_image(sess)
+            load_model = saver.restore(sess,FLAGS.model_dir+ 'model.ckpteye_center-10981')
+
+            labels = sess.run(model_labels,feed_dict={x: image, keep_prob: 1, phase_train: False})
+            Deal_Image.plot_sampleWithLables(image, labels, 0,IFLABELS)
+            #test_data,cols = load_test_data()
+            #generate_submission(test_data,cols,sess,model_labels,x,keep_prob,phase_train)
 
 
 
@@ -90,7 +88,10 @@ def train(if_train):
 
 
 def main(argv=None):
-    train(if_train=True)
+    #datas = pd.read_csv('training.csv')
+    #flip_images(datas)
+    #Deal_Image.DataCenter_eye(datas,0,3)
+    train(if_train=False)
 
 
 
